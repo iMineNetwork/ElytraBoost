a54e73ab3c7b8a8254eb722a3f73d5ed97123f38
Dennis <Dennis.Mulder4@hva.nl>
2017-06-27 16:56:06 +0200

    method to get booster form name or id method to get relative coördinates

--- src/main/java/nl/imine/elytraboost/Util.java
+++ src/main/java/nl/imine/elytraboost/Util.java
@@ -5,6 +5,8 @@
  */
 package nl.imine.elytraboost;
 
+import org.bukkit.Location;
+
 /**
  *
  * @author Dennis
@@ -20,5 +22,57 @@ public class Util {
         value = value * factor;
         long tmp = Math.round(value);
         return (double) tmp / factor;
+    }
+
+    public static ElytraBooster getBooster(String given) {
+        String name = "";
+        int id = 0;
+        boolean usingID = false;
+
+        try {
+            id = Integer.parseInt(given);
+            usingID = true;
+        } catch (NumberFormatException e) {
+            name = given;
+        }
+        ElytraBooster booster = null;
+
+        for (ElytraBooster elytraBooster : ElytraBoosterManager.getInstance().getBoosters()) {
+            if (usingID ? elytraBooster.getID() == id : elytraBooster.getName().equals(name)) {
+                booster = elytraBooster;
+                break;
+            }
+        }
+        return booster;
+    }
+
+    public static Location getRelativePosition(Location locationSender, String offsetX, String offsetY, String offsetZ) throws NumberFormatException {
+        Location location = locationSender;
+
+        if (offsetX.startsWith("~")) {
+            if (!offsetX.equalsIgnoreCase("~")) {
+                location.setX(location.getX() + Double.parseDouble(offsetX.substring(1)));
+            }
+        } else {
+            location.setX(Double.parseDouble(offsetX));
+        }
+
+        if (offsetY.startsWith("~")) {
+            if (!offsetY.equalsIgnoreCase("~")) {
+                location.setY(location.getY() + Double.parseDouble(offsetY.substring(1)));
+            }
+        } else {
+            location.setY(Double.parseDouble(offsetY));
+        }
+
+        if (offsetZ.startsWith("~")) {
+            if (!offsetZ.equalsIgnoreCase("~")) {
+                location.setZ(location.getZ() + Double.parseDouble(offsetZ.substring(1)));
+            }
+        } else {
+            location.setZ(Double.parseDouble(offsetZ));
+        }
+
+        return location;
     }
 }
